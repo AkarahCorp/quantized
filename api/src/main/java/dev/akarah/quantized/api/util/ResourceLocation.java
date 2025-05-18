6b5d8c40b8efa159365c821fa967264e03618cec
akarahdev <endisticbutitsforgit@gmail.com>
2025-05-17 23:23:04 -0400

    feat: add ResourceLocation api

---
+++ api/src/main/java/dev/akarah/quantized/api/util/ResourceLocation.java
@@ -0,0 +1,40 @@
+package dev.akarah.quantized.api.util;
+
+public class ResourceLocation {
+    String namespace;
+    String path;
+
+    private ResourceLocation(String namespace, String path) {
+        this.namespace = namespace;
+        this.path = path;
+    }
+
+    public static ResourceLocation parse(String rawString) {
+        if(!rawString.contains(":")) {
+            rawString = "minecraft:" + rawString;
+        }
+        var split = rawString.split(":");
+        return new ResourceLocation(split[0], split[1]);
+    }
+
+    public static ResourceLocation of(String namespace, String path) {
+        return new ResourceLocation(namespace, path);
+    }
+
+    public String namespace() {
+        return this.namespace;
+    }
+
+    public String path() {
+        return this.path;
+    }
+
+
+    public ResourceLocation withNamespace(String newNamespace) {
+        return new ResourceLocation(newNamespace, path);
+    }
+
+    public ResourceLocation withPath(String newPath) {
+        return new ResourceLocation(namespace, newPath);
+    }
+}
