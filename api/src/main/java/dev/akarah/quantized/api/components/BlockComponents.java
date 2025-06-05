a76612a203bff6c609a084b3190dc09ef584e152
akarahdev <endisticbutitsforgit@gmail.com>
2025-06-04 20:01:46 -0400

    feat: make block components easy to convert

--- api/src/main/java/dev/akarah/quantized/api/components/BlockComponents.java
+++ api/src/main/java/dev/akarah/quantized/api/components/BlockComponents.java
@@ -6,4 +6,7 @@ public class BlockComponents {
     public static DataComponentType<Integer> AGE = DataComponentType.<Integer>builder()
             .named(ResourceLocation.of("minecraft", "age"))
             .build();
+    public static DataComponentType<Boolean> ATTACHED = DataComponentType.<Boolean>builder()
+            .named(ResourceLocation.of("minecraft", "attached"))
+            .build();
 }
--- fabric/src/main/java/dev/akarah/quantized/fabric/convert/BlockStateConversion.java
+++ fabric/src/main/java/dev/akarah/quantized/fabric/convert/BlockStateConversion.java
@@ -2,37 +2,62 @@ package dev.akarah.quantized.fabric.convert;
 
 import dev.akarah.quantized.api.components.BlockComponents;
 import dev.akarah.quantized.api.components.DataComponentMap;
+import dev.akarah.quantized.api.components.DataComponentType;
 import dev.akarah.quantized.fabric.Main;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.Property;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 
 public class BlockStateConversion {
-    @SuppressWarnings("unchecked")
-    public static <T extends Comparable<T>> Property<T> getProperty(BlockState blockState, String property) {
-        return (Property<T>) blockState.getBlock().getStateDefinition().getProperty(property);
-    }
-
-    public static <T extends Comparable<T>> Optional<T> readProperty(BlockState blockState, String property, Class<T> clazz) {
-        var prop = BlockStateConversion.<T>getProperty(blockState, property);
-        if(prop == null) {
-            return Optional.empty();
+    record ConversionRule<Q, M extends Comparable<M>>(Property<M> property, DataComponentType<Q> type, Function<M, Q> fromMc, Function<Q, M> toMc) {
+        public static <M extends Comparable<M>> ConversionRule<M, M> rule(Property<M> property, DataComponentType<M> type) {
+            return new ConversionRule<>(property, type, x -> x, x -> x);
         }
-        return Optional.of(blockState.getValue(prop));
     }
 
-    public static <T extends Comparable<T>> BlockState writeProperty(BlockState blockState, String property, T value) {
-        var prop = BlockStateConversion.<T>getProperty(blockState, property);
-        if(prop == null) {
-            return blockState;
+    static List<ConversionRule<?, ?>> PROPERTY_TO_COMPONENT = List.of(
+            ConversionRule.rule(BlockStateProperties.AGE_1, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_2, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_3, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_4, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_5, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_7, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_15, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.AGE_25, BlockComponents.AGE),
+            ConversionRule.rule(BlockStateProperties.ATTACHED, BlockComponents.ATTACHED)
+    );
+
+    public static <P extends Comparable<P>, C> void readProperty(
+            BlockState blockState,
+            Property<P> property,
+            DataComponentMap.Builder dataComponentMap,
+            DataComponentType<C> type,
+            Function<P, C> convert
+    ) {
+        if(blockState.hasProperty(property)) {
+            dataComponentMap.setUnchecked(type, convert.apply(blockState.getValue(property)));
         }
-        return blockState.setValue(prop, value);
     }
 
+    public static <P extends Comparable<P>, C> void writeProperty(
+            AtomicReference<BlockState> blockState,
+            Property<P> property,
+            DataComponentMap dataComponentMap,
+            DataComponentType<C> dataComponentType,
+            Function<C, P> convert
+    ) {
+        if(blockState.get().hasProperty(property)) {
+            dataComponentMap.get(dataComponentType).ifPresent(value -> blockState.set(blockState.get().trySetValue(property, convert.apply(value))));
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public static BlockState convert(dev.akarah.quantized.api.dimension.BlockState blockState) {
         var block = Main.FABRIC_SERVER.registryAccess().lookup(Registries.BLOCK)
                 .flatMap(x -> x.get(Converters.convert(blockState.blockType())))
@@ -41,11 +66,14 @@ public class BlockStateConversion {
 
         AtomicReference<BlockState> state = new AtomicReference<>(block.defaultBlockState());
 
-        blockState.components().get(BlockComponents.AGE).ifPresent(value -> state.set(writeProperty(state.get(), "age", value)));
+        for(var prop : PROPERTY_TO_COMPONENT) {
+            writeProperty(state, (Property) prop.property(), blockState.components(), (DataComponentType) prop.type(), prop.toMc());
+        }
 
         return state.get();
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public static dev.akarah.quantized.api.dimension.BlockState convert(BlockState blockState) {
         var components = DataComponentMap.builder();
 
@@ -53,7 +81,9 @@ public class BlockStateConversion {
                 .map(x -> x.getKey(blockState.getBlock()))
                 .orElseThrow();
 
-        readProperty(blockState, "age", Integer.class).ifPresent(value -> components.set(BlockComponents.AGE, value));
+        for(var prop : PROPERTY_TO_COMPONENT) {
+            readProperty(blockState, (Property) prop.property(), components, (DataComponentType) prop.type(), prop.fromMc());
+        }
 
         return dev.akarah.quantized.api.dimension.BlockState.of(Converters.convert(blockType))
                 .withComponents(components);
